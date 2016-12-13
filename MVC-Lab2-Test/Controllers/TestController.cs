5b21fb0fb4fbd4a5d0d3f11200a762c78f8a99b8
Toni-PC <Toni-PC@DESKTOP-O39S5M2>
2016-12-13 12:47:54 +0200

    second

--- MVC-Lab2-Test/Controllers/TestController.cs
+++ MVC-Lab2-Test/Controllers/TestController.cs
@@ -10,38 +10,6 @@ namespace MVC_Lab2_Test.Controllers
 {
     public class TestController : Controller
     {
-        //public ActionResult GetView()
-        //{
-        //    Employee emp = new Employee();
-        //    emp.FirstName = "Sukesh";
-        //    emp.LastName = "Marla";
-        //    emp.Salary = 10000;
-
-        //    //ViewData["Employee"] = emp;
-        //    //return View("MyView");
-
-        //    //ViewBag.Employee = emp;
-        //    //return View("MyView", emp);
-
-
-        //    EmployeeViewModel vmEmp = new EmployeeViewModel();
-        //    vmEmp.EmployeeName = emp.FirstName + " " + emp.LastName;
-        //    vmEmp.Salary = emp.Salary.ToString("C");
-        //    if (emp.Salary > 15000)
-        //    {
-        //        vmEmp.SalaryColor = "yellow";
-        //    }
-        //    else
-        //    {
-        //        vmEmp.SalaryColor = "green";
-        //    }
-
-        //    vmEmp.UserName = "Admin";
-
-        //    return View("MyView", vmEmp);
-
-
-        //}
         public ActionResult GetView()
         {
             EmployeeListViewModel employeeListViewModel = new EmployeeListViewModel();
